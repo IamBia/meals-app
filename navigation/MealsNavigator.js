476fe9f24f96edd13d3a79f676e6b27d31edb514
Bianca Schuurman <bia.paulaa@gmail.com>
2021-03-11 11:56:49 +0100

    font fix and icon fix

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,28 +1,47 @@
+import React from 'react';
+
 import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from 'react-navigation';
-import { Platform } from 'react-native'
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons"
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMeals from "../screens/CategoryMealsScreen";
 import MealDetailsScreen from "../screens/MealDetailScreen";
-import Colors from "../constants/Colors"
+import Colors from "../constants/Colors";
+import FavoritesScreen from "../screens/FavoritesScreen";
 
-
-
-
-const MealsNavigator= createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMeals,
-  },
-  MealDetail: MealDetailsScreen,
-}, {
-  defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: {
+      screen: CategoryMeals,
     },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
-});
+    MealDetail: MealDetailsScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+    },
+  }
+);
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: { screen: MealsNavigator, navigationOptions: { tabBarIcon: (tabInfo) => {
+      return <Ionicons name="ios-restaurant" size={25} />
+    } } },
+    Favorites: FavoritesScreen,
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accent,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
